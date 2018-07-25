41831b0267a8f714af9420a46ac83e211b409d76
766aya <1494233259@qq.com>
2018-07-25 15:22:45 +0800

    reupdate

--- Demo.js
+++ Demo.js
@@ -1,1 +1,27 @@
-Demo
+// let user = require('./User');
+
+// console.log(`userName:${user.userName}`)
+// console.log(`I'm ${user.userName}, I say ${user.sayHello()}`)
+
+
+let http = require('http');
+let url = require('url');
+let util = require('util');
+
+var server = http.createServer((req, res)=>{
+	res.statusCode = 200
+	res.setHeader("Content-Type", "text/plain; charset=utf-8")
+	
+	// console.log("url: ", req.url) // 字符串
+	// console.log("parse: ", url.parse(req.url)) // [object]
+	console.log('util.inspect: ', util.inspect(url.parse(req.url))) // 转字符串 调试使用
+	//请求结束的返回值
+	res.end(util.inspect(url.parse(req.url)))
+})
+
+let HOST = "127.0.0.1"
+let PORT = 8888
+
+server.listen(PORT, HOST, (res)=>{
+	console.log(`服务器已经运行，请访问： ${HOST}:${PORT}`)
+})
--- User.js
+++ User.js
@@ -1,6 +1,14 @@
+// 类似于exports.default 导出一个对象
 module.exports = {
 	userName: "Jack",
-	sayHello: function () {
+	sayHello () {
 		return 'Hello!'
 	}
 }
+
+
+// 暴露一个对象
+// exports.userName = "Tom"
+// exports.sayHello = function() {
+// 	return 'Hello World!'
+// }
